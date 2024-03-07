89f8df113f365f8bce488fd7c4c252b858178c7b
SirDiondi <danschmidtbc@gmail.com>
2024-03-07 11:38:20 -0800

    Created a random int generator function.
    Created a computer choice function, which uses
    the output from the random int generator to
    pick an option of rock, paper, or scissors.
    
    Outputs assigned selection.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+    //Created random int function to be used later in deciding computer choice
+function getRandomInt(max) {
+    // get a random float between  0 - 1, multiply it by our input, then set it to floor.
+    return Math.floor(Math.random() * max);
+}
+
+    //Created function to decide computer choice
+function getComputerChoice() {
+    //store our rock paper scissors variables, plus the computers choice
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+    const computerChoice = getRandomInt(3);
+    // set our result from computerChoice to a specified text output based on the numerical input
+    if (computerChoice === 0) {
+        return rock;
+    }
+    else if (computerChoice === 1) {
+        return  paper;
+    }
+    else if (computerChoice === 2) {
+        return  scissors;
+    }
+}
+
+console.log( getComputerChoice());
