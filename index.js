a8d390258c57bbd093c93b5ba569759f026e7c2f
SirDiondi <danschmidtbc@gmail.com>
2024-03-08 07:53:02 -0800

    changed the playRound and added playGame function Changed the playRound function so that it no longer just console logs the result of the round. Now it should log it, but also return the result as a number, 0 = tie, 1 = lose, 2 = win.
    Created a playGame function, which plays 5 playRound rounds, incrementing loses and wins depending on the result.
    After 5 rounds, we decide a winner, or a tie if need be, and we return this result to the player.

--- index.js
+++ index.js
@@ -34,25 +34,65 @@ function getPlayerChoice(playerChoice) {
     // this function compares two inputs, player selection and the computers selection, it checks if they are
     // equal, or if the player or the computer wins.
 function playRound(playerSelection, computerSelection) {
+    const winCondition = `You chose ${playerSelection}! I chose ${computerSelection}! you won this round!`;
+    const loseCondition = `You chose ${playerSelection}! I chose ${computerSelection}! you lost this round!`;
+    const tieCondition = `You chose ${playerSelection}! I chose ${computerSelection}! hey, we tied!`;
+
     // check if the player has lost by comparing all possibilities options.
     // then return result.
     if ((playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "rock")) {
-        return `You chose ${playerSelection}! I chose ${computerSelection}! Sorry, you lose!`;
+        console.log(loseCondition);
+        return "lose";
     }
     // check if the player has lost by comparing all possibilities options.
     // then return result.
     else if ((computerSelection === "rock" && playerSelection === "paper")||(computerSelection === "paper" && playerSelection === "scissors")||(computerSelection === "scissors" && playerSelection === "rock")) {
-        return `You chose ${playerSelection}! I chose ${computerSelection}! Congratulations, you won!`;
+        console.log(winCondition);
+        return "win";
     }
     // if your choice is the same as the computers, return that we tied.
     else if (playerSelection === computerSelection) {
-        return `You chose ${playerSelection}! I chose ${computerSelection}! hey, we tied!`;
+        console.log(tieCondition);
+        return "tie";
     }
     // include an option for handing errors relating to the player inputting something that isn't
     // "rock", "paper", or "scissors".
     else {
-        return "sorry, incorrect input. You must input rock, paper, or scissors.";
+        playRound(getPlayerChoice(prompt("sorry, incorrect input. You must input rock, paper, or scissors: ")), computerSelection);
     }
 }
 
-console.log(playRound(getPlayerChoice(prompt("Please choose an option, rock, paper, or scissors: ")), getComputerChoice()));
+function playGame() {
+    // create some variables to store our wins and loses
+    let gameWins = 0;
+    let gameLoses = 0
+    // run 5 rounds of playRound and each win or lose store that result in the apropriate variable.
+    for (let i = 0; i < 5; i++) {
+        const playResult = playRound(getPlayerChoice(prompt("Please choose an option, rock, paper, or scissors: ")), getComputerChoice());
+        if (playResult === "lose") {
+            gameLoses+= 1;
+        }
+        else if (playResult === "win") {
+            gameWins+= 1;
+        }
+        // I think we could just not have an option for ties, but I included this else here for if
+        // a tie did happen, since a tie does not have a winner it should just do nothing.
+        else {
+        }
+    }
+    // once we have our results from the 5 games, we must report the results to the player.
+    // if game loses are greater than game wins, report we lost.
+    if (gameLoses > gameWins) {
+        console.log(`that's ${gameLoses} for me, and ${gameWins} for you, sorry, I win the game!`);
+    }
+    // if game wins are greater than game loses, report we won.
+    else if (gameLoses < gameWins) {
+        console.log(`that's ${gameLoses} for me, and ${gameWins} for you, damn! congratulations, you win the game!`);
+    }
+    // if the results are equal, report we tied.
+    else if (gameLoses === gameWins) {
+        console.log(`that's ${gameLoses} for me, and ${gameWins} for you, looks like we tied the game!`);
+    }
+
+}
+playGame();
