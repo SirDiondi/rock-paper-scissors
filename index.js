60c66f3a8d29a49caa6d8e659991f65882dfa4e7
SirDiondi <danschmidtbc@gmail.com>
2024-04-08 05:50:47 -0700

    Changed it so the game has actual rounds

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+const content = document.getElementById("content");
+const textLog = document.getElementById("textLog");
 
 //Created random int function to be used later in deciding computer choice
 function getRandomInt(max) {
@@ -41,28 +42,157 @@ function playRound(playerSelection, computerSelection) {
     // check if the player has lost by comparing all possibilities options.
     // then return result.
     if ((playerSelection === "rock" && computerSelection === "paper")||(playerSelection === "paper" && computerSelection === "scissors")||(playerSelection === "scissors" && computerSelection === "rock")) {
-        console.log(loseCondition);
+        textLog.textContent = loseCondition;
         return "lose";
     }
     // check if the player has lost by comparing all possibilities options.
     // then return result.
     else if ((computerSelection === "rock" && playerSelection === "paper")||(computerSelection === "paper" && playerSelection === "scissors")||(computerSelection === "scissors" && playerSelection === "rock")) {
-        console.log(winCondition);
+        textLog.textContent = winCondition;
         return "win";
     }
     // if your choice is the same as the computers, return that we tied.
     else if (playerSelection === computerSelection) {
-        console.log(tieCondition);
+        textLog.textContent = tieCondition;
         return "tie";
     }
     // include an option for handing errors relating to the player inputting something that isn't
     // "rock", "paper", or "scissors".
     else {
-        playRound(getPlayerChoice(prompt("sorry, incorrect input. You must input rock, paper, or scissors: ")), computerSelection);
+        playRound(getPlayerChoice(textLog.textContent = "sorry, incorrect input. You must input rock, paper, or scissors: "), computerSelection);
     }
 }
 
-function playGame() {
+const button1 = document.createElement("button");
+button1.textContent = "rock";
+
+const button2 = document.createElement("button");
+button2.textContent = "paper";
+
+const button3 = document.createElement("button");
+button3.textContent = "scissors";
+
+content.appendChild(button1);
+content.appendChild(button2);
+content.appendChild(button3);
+
+const playerNumber = document.createElement("span");
+playerNumber.textContent = "Player:";
+content.appendChild(playerNumber);
+const playerTracker = document.createElement("span");
+content.appendChild(playerTracker);
+const computerNumber = document.createElement("span");
+computerNumber.textContent = "Computer:";
+content.appendChild(computerNumber);
+const computerTracker = document.createElement("span");
+content.appendChild(computerTracker);
+
+function buttonPlayGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+    let currentRound = undefined;
+    
+    button1.addEventListener("click", () => {
+        currentRound = playRound(getPlayerChoice("rock"), getComputerChoice())
+        
+        if (currentRound === "win") {
+            playerWins += 1;
+            playerTracker.textContent = playerWins;
+            if (playerWins >= 5) {
+                textLog.textContent = "Congratulations! you won!";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }  
+        else if (currentRound === "lose") {
+            computerWins += 1;
+            computerTracker.textContent = computerWins;
+            if (computerWins >= 5) {
+                textLog.textContent = "Sorry! you lost.";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }
+        else {
+            playerWins += 0;
+            playerTracker.textContent = playerWins;
+            computerWins += 0;
+            computerTracker.textContent = computerWins;
+        }
+    })
+    button2.addEventListener("click", () => {
+        currentRound = playRound(getPlayerChoice("paper"), getComputerChoice())
+        if (currentRound === "win") {
+            playerWins += 1;
+            playerTracker.textContent = playerWins;
+            if (playerWins >= 5) {
+                textLog.textContent = "Congratulations! you won!";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }  
+        else if (currentRound === "lose") {
+            computerWins += 1;
+            computerTracker.textContent = computerWins;
+            if (computerWins >= 5) {
+                textLog.textContent = "Sorry! you lost.";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }
+        else {
+            playerWins += 0;
+            playerTracker.textContent = playerWins;
+            computerWins += 0;
+            computerTracker.textContent = computerWins;
+        }
+    })
+    button3.addEventListener("click", () => {
+        currentRound = playRound(getPlayerChoice("scissors"), getComputerChoice())
+        if (currentRound === "win") {
+            playerWins += 1;
+            playerTracker.textContent = playerWins;
+            if (playerWins >= 5) {
+                textLog.textContent = "Congratulations! you won!";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }  
+        else if (currentRound === "lose") {
+            computerWins += 1;
+            computerTracker.textContent = computerWins;
+            if (computerWins >= 5) {
+                textLog.textContent = "Sorry! you lost.";
+                playerWins = 0;
+                computerWins = 0;
+                currentRound = undefined;
+                return;
+            }
+        }
+        else {
+            playerWins += 0;
+            playerTracker.textContent = playerWins;
+            computerWins += 0;
+            computerTracker.textContent = computerWins;
+        }
+    })
+    
+    
+}
+
+buttonPlayGame();
+
+/*function playGame() {
     // create some variables to store our wins and loses
     let gameWins = 0;
     let gameLoses = 0
@@ -95,4 +225,4 @@ function playGame() {
     }
 
 }
-playGame();
+playGame();*/
